936422fc7e8e17d14ee9fb942d9017aec68bfa2b
Lu, Richard <rlu@Collegeboard.org>
2020-05-27 20:08:37 -0400

    enter

--- src/App.js
+++ src/App.js
@@ -13,15 +13,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome!
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://rich2000.shopify.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Enter the Contax Care store
           </a>
         </header>
       </div>
